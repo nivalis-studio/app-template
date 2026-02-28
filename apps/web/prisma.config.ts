ff7439084f81af855ddcc307360825b5320166dc
Paul Nodet <5941125+pnodet@users.noreply.github.com>
2026-03-01 00:14:15 +0100

    chore: bump deps

--- apps/web/prisma.config.ts
+++ apps/web/prisma.config.ts
@@ -1,10 +1,10 @@
 import 'dotenv/config';
-import { defineConfig, env } from 'prisma/config';
+import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
   schema: 'src/prisma/schema',
   migrations: { path: 'src/prisma/migrations' },
   views: { path: 'src/prisma/views' },
   typedSql: { path: 'src/prisma/queries' },
-  datasource: { url: env('DATABASE_URL') },
+  datasource: { url: './database.sqlite' },
 });
